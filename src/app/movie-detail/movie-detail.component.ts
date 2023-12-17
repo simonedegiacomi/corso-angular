10df2e01e1d08c2549ddd069b186feb17af9c89e
Simone Degiacomi <simonedegiacomi97@gmail.com>
2023-12-17 18:32:12 +0100

    aggiunge form per review

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../movie';
 import { MovieService } from '../movie.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ReviewService } from '../review.service';
 
 @Component({
   selector: 'app-movie-detail',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   template: `
     <article>
       <img class="movie-poster" [src]="movie?.poster">
@@ -25,6 +27,20 @@ import { MovieService } from '../movie.service';
           <li>IMDB rating: {{ movie?.imdbRating }} / 10</li>
         </ul>
       </section>
+
+      <section class="movie-insert-review">
+        <h2>Add your review</h2>
+        
+        <form [formGroup]="reviewForm" (ngSubmit)="submitReview()">
+          <label for="nickname">Nickname: </label>
+          <input id="nickname" type="text" formControlName="nickname">
+
+          <label for="review">Review: </label>
+          <textarea id="review" rows="3" formControlName="review"></textarea>
+
+          <button type="submit" class="primary">Send</button>
+        </form>
+      </section>
     </article>
   `,
   styleUrl: './movie-detail.component.css'
@@ -35,6 +51,12 @@ export class MovieDetailComponent {
   private readonly movieService = inject(MovieService)
   movie: Movie | null = null;
 
+  reviewForm = new FormGroup({
+    nickname: new FormControl(""),
+    review: new FormControl("")
+  });
+  reviewService = inject(ReviewService);
+
   constructor() {
     const movieId = parseInt(this.route.snapshot.params['id'])
     this
@@ -42,6 +64,16 @@ export class MovieDetailComponent {
     .getMovieById(movieId)
     .then(movie => {
       this.movie = movie;
+    });
+  }
+
+  submitReview() {
+    this.reviewService.createReview(
+      this.reviewForm.value.nickname ?? "",
+      this.reviewForm.value.review ?? ""
+    ).then(() => {
+      this.reviewForm.reset();
+      alert("Review submitted!");
     });
   }
 }
---
+++ src/app/review.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+  private readonly client = axios.create();
+
+  constructor() { }
+
+  async createReview(nickname: string, review: string) {
+    await this.client.post("http://localhost:3000/reviews", {
+      nickname, review
+    });
+  }
+}
